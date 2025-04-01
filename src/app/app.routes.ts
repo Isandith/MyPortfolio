6e1f33e1cc0c0ea81ee1e6eb6cce14ce55f0b45a
isandith perera <isandith.20230232@iit.ac.lk>
2025-04-01 23:03:48 +0530

    feat: creating nav bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+import { ContactsComponent } from './contacts/contacts.component';
+import { ResumeComponent } from './resume/resume.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home',
+    component:HomeComponent
+    },
+    {path:'portfolio',
+        component:PortfolioComponent
+    },
+    {path:'contact',
+    component:ContactsComponent
+    },
+    {path:'resume',
+        component:ResumeComponent
+    },
+
+    {path:'**',component:HomeComponent,
+        pathMatch:'full'
+
+    }
+    
+
+
+];
